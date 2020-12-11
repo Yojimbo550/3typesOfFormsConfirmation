1628306f2f34778b2e18b212ee9ce042b08b6e7e
Yojimbo <alexyeryomin13@gmail.com>
2020-12-11 15:31:44 +0300

    Some work

--- src/main/java/testgroup/firstproduct/controller/CustomerController.java
+++ src/main/java/testgroup/firstproduct/controller/CustomerController.java
@@ -1,8 +1,11 @@
 package testgroup.firstproduct.controller;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import testgroup.firstproduct.Customer;
@@ -12,6 +15,11 @@ import javax.validation.Valid;
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder) {
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
     @RequestMapping("/showForm")
     public String showForm(Model theModel) {
     theModel.addAttribute("customer" , new Customer());
